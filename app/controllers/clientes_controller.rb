349f63eeed3d8dd7c7eb4fe1363f8d5417a757d0
Tiago Moraes <tiago_h_moraes@hotmail.com>
2018-04-09 18:33:24 -0300

    Implementando Cadastro e Listagem

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,9 +1,22 @@
 class ClientesController < ApplicationController
 
 	def index
-		@clientes = Cliente.all
 	end
 	def new
+		@cliente = Cliente.new
 	end
-	
+	def create
+		@cliente = Cliente.new params.require(:cliente).permit :nome, :idade, :cidade
+		if @cliente.save
+			flash[:notice] = "Cliente salvo com sucesso"
+			redirect_to root_url
+		end
+	end
+
+	def show
+		@cliente = Cliente.all
+	end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  	resource :cliente, only: [:nome, :idade, :cidade]
+  	post "/clientes" => "clientes#create"
   	get "/clientes/new" => "clientes#new"
+  	get "/clientes/show" => "clientes#show", as: :clientes_show
   # get "/clientes"
  	root "clientes#index"
 end
