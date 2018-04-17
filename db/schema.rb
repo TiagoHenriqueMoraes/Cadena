eebd594e746b7459da6de2cbf55f208872f3d01b
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-04-17 15:33:04 -0300

    Atualizado postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180409204440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clientes", force: :cascade do |t|
     t.string "nome"
     t.integer "idade"
