b8e7c0c4c6990b0de032a0bb66f2c40f56346047
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-10-09 11:59:46 -0300

    fixes rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
 
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -19,12 +19,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
-
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
 end
